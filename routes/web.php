da9b878bb113969a0b47016daba1c14e291e5c10
Erik Bernskiold <erik@bernskiold.com>
2025-05-31 19:48:46 +0200

    Name the routes

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,5 @@
 
 use BernskioldMedia\Fortnox\Controllers\FortnoxAuthController;
 
-Route::get(config('fortnox.routes.oauth.redirect'), [FortnoxAuthController::class, 'toFortnox']);
-Route::get(config('fortnox.routes.oauth.callback'), [FortnoxAuthController::class, 'handleCallback']);
+Route::get(config('fortnox.routes.oauth.redirect'), [FortnoxAuthController::class, 'toFortnox'])->name('fortnox.oauth.redirect');
+Route::get(config('fortnox.routes.oauth.callback'), [FortnoxAuthController::class, 'handleCallback'])->name('fortnox.oauth.callback');
