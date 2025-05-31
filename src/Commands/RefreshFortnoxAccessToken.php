e898d3095529d886495adc2209e8633fa55c33d1
Erik Bernskiold <erik@bernskiold.com>
2025-05-31 21:35:48 +0200

    wip

--- src/Commands/RefreshFortnoxAccessToken.php
+++ src/Commands/RefreshFortnoxAccessToken.php
@@ -4,6 +4,8 @@ namespace BernskioldMedia\Fortnox\Commands;
 
 use BernskioldMedia\Fortnox\Contracts\TokenStorage;
 use Illuminate\Console\Command;
+use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Two\Token;
 use function app;
 use function config;
 
@@ -22,10 +24,28 @@ class RefreshFortnoxAccessToken extends Command
          */
         $storageProvider = app(config('fortnox.storage_provider'));
 
-        if (!$storageProvider->hasToken()) {
+        $token = $storageProvider->getToken();
+
+        if (!$token) {
             $this->error('No access token found. Please authenticate with Fortnox first.');
             return self::FAILURE;
         }
+
+        if (now()->lessThan($token->expiresAt) && !$this->option('force')) {
+            $this->info('Access token is still valid. No need to refresh. Use with --force to refresh anyway.');
+            return self::SUCCESS;
+        }
+
+        /**
+         * @var Token $newToken
+         */
+        $newToken = Socialite::driver('fortnox')->refreshToken($token->refreshToken);
+
+        $storageProvider->storeToken($newToken);
+
+        $this->info('Access token refreshed successfully.');
+
+        return self::SUCCESS;
     }
 
 }
